e9ebc670eafb322e033aacc366069a6716290d35
Zyon Xiong <xiong.houa95@gmail.com>
2021-04-23 03:39:35 +0000

    finished every other items

--- arrays.js
+++ arrays.js
@@ -3,14 +3,25 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
-}
 
+  // for (const item of items) { //give indices
+  //   console.log(item);
+  for (const index in items) {
+    console.log(items[index], index);
+    }
+  }
+
+//printIndices(['apple','berry','cherry'])
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+
+  for (let i = 0; i < items.length; i += 2) { //this is a way to create a counter
+    console.log(items[i]);
+  }
 }
+
+everyOtherItem(['apple','berry','cherry', 'durian'])
 
 
 // 3. smallestNItems
